cb49431f5b6a5056cc3b7fa8f87189ecdd94f32d
Ali Asfar <asfar.wpbrigade@gmail.com>
2025-08-26 13:49:57 +0500

    First change in main branch.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
+# Initialize the Flask application
 app = Flask(__name__)
 
+# Define a simple route
 @app.route("/")
 def hello_world():
-    return "Hello, World! This is my Flask GitFlow demo.First Change."
+    return "Hello, World! 👋 This is my Flask GitFlow demo. First change in main branch."
 
+# Entry point for running the app directly
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # Run the app on all interfaces, port 5000, with debug enabled
+    app.run(host="0.0.0.0", port=5000, debug=True)
