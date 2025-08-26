d8e4274b02a2123ed9ad6b4318f881d3b35e79d4
Ali Asfar <asfar.wpbrigade@gmail.com>
2025-08-26 13:01:40 +0500

    First chhange

--- app.py
+++ app.py
@@ -4,17 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World! This is my Flask GitFlow demo."
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "Hello, World! This is my Flask GitFlow demo."
+    return "Hello, World! This is my Flask GitFlow demo.First Change."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
