86e8bad73fffae4e473608613e97ee005d1ad2a3
Ali Asfar <asfar.wpbrigade@gmail.com>
2025-08-26 13:07:51 +0500

    First Change in Develop Branch

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World! This is my Flask GitFlow demo.First Change."
+    return "Hello, World! This is my Flask GitFlow demo.First Change in develop branch."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
