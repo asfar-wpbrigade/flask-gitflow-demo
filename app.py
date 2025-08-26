4d27a3ac902e4842b5fedd745dd993912b222899
Ali Asfar <asfar.wpbrigade@gmail.com>
2025-08-26 13:16:55 +0500

    Second change to test the yaml file.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World! This is my Flask GitFlow demo.First Change in develop branch."
+    return "Hello, World! This is my Flask GitFlow demo.Second Change in develop branch to test the YAML File."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
